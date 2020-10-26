284be8bc11fbd7efcb2e9aa537eb3a2cd890d145
PapaBearDoes <73200436+PapaBearDoes@users.noreply.github.com>
2020-10-25 19:38:46 -0600

    Setting up for the big changes.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,10 +1,10 @@
 -- Please use the Localization App on Curseforge to Update this
--- http://www.wowace.com/projects/***/localization/
+-- https://www.curseforge.com/wow/addons/dadgratz/localization
 
 local debug = false
   --@debug@
   debug = true
   --@end-debug@
 
-local L = LibStub("AceLocale-3.0"):NewLocale("AddonStub", "enUS", true, debug)
+local L = LibStub("AceLocale-3.0"):NewLocale("DadGratz", "enUS", true, debug)
   --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true)@
