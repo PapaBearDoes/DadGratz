4cabde3514c658a663717186666fe4767f161fcd
PapaBearDoes <73200436+PapaBearDoes@users.noreply.github.com>
2020-11-03 13:04:03 -0700

    working on configuration

--- DadGratz.lua
+++ DadGratz.lua
@@ -15,6 +15,44 @@ local L = addon:GetLocale()
 -- End Imports
 --[[ ######################################################################## ]]
 --   ## Do All The Things!!!
+-- Define Globals
+DG_globals = {
+  enableTasks = {},
+}
+
+-- Create any required hidden frames
+
+-- Create DB defaults
+DG_dbDefaults = {
+  profile = {
+    modules = {
+      ["*"] = 3
+    }
+  }
+}
+
+--Create config defaults
+DG_options = {
+  name = me,
+  desc = "",
+  type = "group",
+  childGroups = "tab"
+  args = {
+    genTab = {
+      order = 1,
+      name = L["GeneralSettings"],
+      type = "group",
+      args = {
+        order = 1,
+        name = "",
+        type = "group",
+        args = {
+          
+        },
+      },
+    },
+  },
+}
 
 --[[
      ########################################################################
